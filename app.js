c8f6516d1e8ac358c604acdcd1e341688f20d802
DOMAA21 <clashecurry21@gmail.com>
2024-03-06 04:29:57 +0800

    origin changed

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
   // origin: 'http://localhost:3000', 
   origin: ['http://localhost:3000','http://localhost:3001','https://treatsdelight.vercel.app','https://octopus-app-bwpel.ondigitalocean.app'], 
-  credentials: true
+  credentials: true,
 }));
 
 app.use(express.json());
