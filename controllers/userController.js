c4bed34ad113ccbd477ba110f064b4af35025615
DOMAA21 <clashecurry21@gmail.com>
2024-03-08 21:07:02 +0800

    rewrite error handlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -311,24 +311,24 @@ exports.loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ErrorHandler("Please enter email & password", 400));
+    return res.status(400).json({ success: false, message: "Please enter email & password" });
   }
 
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
-    return next(new ErrorHandler("Invalid Email or Password", 401));
+    return res.status(401).json({ success: false, message: "Invalid Email or Password" });
   }
 
   const isPasswordMatched = await user.comparePassword(password);
 
   if (!isPasswordMatched) {
-    return next(new ErrorHandler("Invalid Email or Password", 401));
+    return res.status(401).json({ success: false, message: "Invalid Email or Password" });
   }
 
-  const token = user.getJwtToken();
   sendToken(user, 200, res);
 };
+
 
 
 exports.googlelogin = async (req, res, next) => {
