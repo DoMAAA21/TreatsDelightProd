0a42f3ba466218ad83cb0814cd4e025f8f30ec08
DOMAA21 <clashecurry21@gmail.com>
2024-03-19 11:09:40 +0800

    fix middlewareBug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,26 +4,33 @@ const jwt = require("jsonwebtoken");
 
 const ErrorHandler = require("../utils/errorHandler");
 
-// Checks if user is authenticated or not
 
 exports.isAuthenticatedUser = async (req, res, next) => {
   let token;
 
   if (req.headers.authorization) {
     token = req.headers.authorization;
-  }else if (req.cookies.token) {
+  } else if (req.cookies.token) {
     token = req.cookies.token;
   }
 
   if (!token) {
-    return next(new ErrorHandler("Login first to access this resource.", 401));
+    return res.status(401).json({ error: "Login first to access this resource." });
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  req.user = await User.findById(decoded.id);
+    req.user = await User.findById(decoded.id);
 
-  next();
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: "Token expired. Please log in again." });
+    } else {
+      return res.status(401).json({ error: "Invalid token." });
+    }
+  }
 };
 
 exports.authorizeRoles = (...roles) => {
